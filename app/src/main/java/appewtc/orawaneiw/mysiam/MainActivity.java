5365a86e14d42a7082200f183d9d8767f7b68fe4
Admin <ewza555@gmail.com>
2017-06-18 11:43:52 +0700

    Inten to register

--- app/src/main/java/appewtc/orawaneiw/mysiam/MainActivity.java
+++ app/src/main/java/appewtc/orawaneiw/mysiam/MainActivity.java
@@ -1,13 +1,57 @@
 package appewtc.orawaneiw.mysiam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initial View
+        initialView();
+
+        //Text View Controller
+        textViewController();
+
+
+    }   //Mathod Main
+
+    private void textViewController() {
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Intent to NewRegisterActivity
+                Intent intent = new Intent(MainActivity.this, NewRegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
-}
+
+    private void initialView() {
+
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+
+
+    }
+}   //Main Class
+
